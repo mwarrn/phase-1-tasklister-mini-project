7f7c2039fc5d33856cbc81eb4ad3d391bb5b7e00
Michael Warren <mwarrn@amazon.com>
2022-05-07 23:36:18 -0700

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("form");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    buildToDo(event.target.querySelector("#new-task-description").value);
+    form.reset();
+  });
 });
+
+function buildToDo(todo) {
+  const p = document.createElement("p");
+  const button = document.createElement("button");
+  
+  button.addEventListener("click", handleDelete);
+  button.textContent = "❌";
+
+  p.textContent = `${todo}`;
+
+  p.appendChild(button);
+
+  document.querySelector("#list").appendChild(p);
+}
+
+function handleDelete(event) {
+  event.target.parentNode.remove();
+}
